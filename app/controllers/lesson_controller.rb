5e89b5d9a717fde7f839abf4d0462fd1d5f41583
jenmccarthy <jennifernward@gmail.com>
2014-08-28 12:01:38 -0700

    Create CRUD for UI for lesson class

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -8,4 +8,40 @@ class LessonController < ApplicationController
   def new
     render('lessons/new.html.erb')
   end
+
+  def create
+    @lesson = Lesson.new(params[:lesson])
+    if @lesson.save
+      redirect_to("/lessons/")
+    else
+      render('lessons/new.html.erb')
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+    render('lessons/show.html.erb')
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render('lessons/edit.html.erb')
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(params[:lesson])
+      redirect_to("/lessons/#{@lesson.id}/show")
+    else
+      render("lessons/edit.html.erb")
+    end
+  end
+
+  def delete
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to("/lessons/")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   match('/', { :via => :get, :to => 'lesson#index'})
   match('lessons', { :via => :get, :to => 'lesson#index'})
   match('lessons/new', {:via => :get, :to => 'lesson#new'})
+  match('lessons', { :via => :post, :to => 'lesson#create'})
+  match('lessons/:id/show', { :via => :get, :to => 'lesson#show'})
+  match('lessons/:id/edit', {:via => :get, :to => 'lesson#edit'})
+  match('lessons/:id/', {:via => [:patch, :put], :to => 'lesson#update'})
+  match('lessons/:id', {:via => :delete, :to => 'lesson#delete'})
 end
