42ab6a080d85917f42e1931c7d653fa119d2250e
jenmccarthy <jennifernward@gmail.com>
2014-08-28 14:55:28 -0700

    Create method to edit section name

--- app/controllers/section_controller.rb
+++ app/controllers/section_controller.rb
@@ -10,4 +10,18 @@ class SectionController < ApplicationController
     render('sections/new.html.erb')
   end
 
+  def create
+    @section = Section.new(params[:section])
+    if @section.save
+      redirect_to("/sections/")
+    else
+      render('sections/new.html.erb')
+    end
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+    render('sections/edit.html.erb')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match('/', { :via => :get, :to => 'lesson#index'})
+  match('/', { :via => :get, :to => 'section#index'})
   match('lessons', { :via => :get, :to => 'lesson#index'})
   match('lessons/new', {:via => :get, :to => 'lesson#new'})
   match('lessons', { :via => :post, :to => 'lesson#create'})
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   match('sections', { :via => :get, :to => 'section#index'})
   match('sections/new', { :via => :get, :to => 'section#new'})
+  match('sections', { :via => :post, :to => 'section#create'})
+  match('sections/:id/edit', { :via => :get, :to => 'section#edit'})
 
 
 end
